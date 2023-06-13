5cbe7b7fca03843c1d7aa182a84d2eace2535a37
ThayssaFerreira <120604469+ThayssaFerreira@users.noreply.github.com>
2023-06-13 16:21:18 +0000

    Pdf funcionando

--- script.js
+++ script.js
@@ -1,154 +1,201 @@
-// Variáveis do cronômetro
-let timerInterval;
-let timerRunning = false;
-let timerStartTime;
-let timerDisplay = document.getElementById('timer-display');
+// Variáveis globais
+let pdfDoc = null;
+let pageNum = 1;
+let pageRendering = false;
+let pageNumPending = null;
+let scale = 1.5;
 
-// Variáveis da calculadora
-let calcInput = document.getElementById('calc-input');
-let calcButtons = document.querySelectorAll('#calc-buttons button');
+// Elementos DOM
+const pdfContainer = document.getElementById('pdf-container');
+const fileInput = document.getElementById('file-input');
+const darkModeButton = document.getElementById('dark-mode-button');
+const clock = document.getElementById('clock');
+const checklistInput = document.getElementById('checklist-input');
+const addItemButton = document.getElementById('add-item-button');
+const checklistItems = document.getElementById('checklist-items');
+const startTimerButton = document.getElementById('start-timer-button');
+const stopTimerButton = document.getElementById('stop-timer-button');
+const resetTimerButton = document.getElementById('reset-timer-button');
+const timerDisplay = document.getElementById('timer-display');
+const calcInput = document.getElementById('calc-input');
+const calcButtons = document.getElementById('calc-buttons');
 
-// Função para exibir a hora atual
-function updateTime() {
+// Carrega o arquivo PDF selecionado
+fileInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  const fileReader = new FileReader();
+
+  fileReader.onload = function() {
+    const typedarray = new Uint8Array(this.result);
+    loadPdf(typedarray);
+  };
+
+  fileReader.readAsArrayBuffer(file);
+});
+
+// Carrega o PDF a partir de um array de bytes
+function loadPdf(typedarray) {
+  pdfjsLib.getDocument(typedarray).promise.then(function(pdfDoc_) {
+    pdfDoc = pdfDoc_;
+    renderPage(pageNum);
+  });
+}
+
+// Renderiza a página PDF especificada
+function renderPage(num) {
+  pageRendering = true;
+  pdfDoc.getPage(num).then(function(page) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const viewport = page.getViewport({ scale: scale });
+
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderContext = {
+      canvasContext: ctx,
+      viewport: viewport
+    };
+
+    const renderTask = page.render(renderContext);
+
+    renderTask.promise.then(function() {
+      pageRendering = false;
+
+      if (pageNumPending !== null) {
+        renderPage(pageNumPending);
+        pageNumPending = null;
+      }
+    });
+
+    pdfContainer.innerHTML = '';
+    pdfContainer.appendChild(canvas);
+  });
+
+  // Atualiza o número da página atual
+  document.getElementById('page-num').textContent = num;
+}
+
+// Vai para a próxima página
+function nextPage() {
+  if (pageRendering) {
+    pageNumPending = pageNum + 1;
+  } else {
+    pageNum++;
+    if (pageNum > pdfDoc.numPages) {
+      pageNum = pdfDoc.numPages;
+    }
+    renderPage(pageNum);
+  }
+}
+
+// Vai para a página anterior
+function prevPage() {
+  if (pageRendering) {
+    pageNumPending = pageNum - 1;
+  } else {
+    pageNum--;
+    if (pageNum < 1) {
+      pageNum = 1;
+    }
+    renderPage(pageNum);
+  }
+}
+
+// Alterna entre o modo claro e escuro
+darkModeButton.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
+
+// Atualiza o relógio a cada segundo
+function updateClock() {
   const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  const time = `${hours}:${minutes}:${seconds}`;
-  document.getElementById('clock').textContent = time;
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  clock.textContent = `${hours}:${minutes}:${seconds}`;
 }
+setInterval(updateClock, 1000);
 
-// Função para iniciar o cronômetro
-function startTimer() {
-  if (!timerRunning) {
-    timerStartTime = Date.now();
-    timerInterval = setInterval(updateTimerDisplay, 1000);
-    timerRunning = true;
-  }
-}
-
-// Função para parar o cronômetro
-function stopTimer() {
-  if (timerRunning) {
-    clearInterval(timerInterval);
-    timerRunning = false;
-  }
-}
-
-// Função para reiniciar o cronômetro
-function resetTimer() {
-  stopTimer();
-  timerDisplay.textContent = '00:00:00';
-}
-
-// Função para atualizar a exibição do cronômetro
-function updateTimerDisplay() {
-  const currentTime = Date.now();
-  const elapsedTime = currentTime - timerStartTime;
-  const hours = Math.floor(elapsedTime / 3600000).toString().padStart(2, '0');
-  const minutes = Math.floor((elapsedTime % 3600000) / 60000).toString().padStart(2, '0');
-  const seconds = Math.floor((elapsedTime % 60000) / 1000).toString().padStart(2, '0');
-  timerDisplay.textContent = `${hours}:${minutes}:${seconds}`;
-}
-
-
-// Função para adicionar um item à checklist
+// Adiciona um item à lista de verificação
 function addItem() {
-  const input = document.getElementById('checklist-input');
-  const itemText = input.value.trim();
+  const itemText = checklistInput.value.trim();
 
   if (itemText !== '') {
     const item = document.createElement('li');
     item.textContent = itemText;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Remover';
-    deleteButton.addEventListener('click', function () {
-      item.remove();
-    });
-
-    item.appendChild(deleteButton);
-    document.getElementById('checklist-items').appendChild(item);
-    input.value = '';
+    checklistItems.appendChild(item);
+    checklistInput.value = '';
   }
 }
 
-
-
-// Função para alternar a visibilidade do cronômetro
-function toggleTimer() {
-  const timer = document.getElementById('timer');
-  timer.classList.toggle('hidden');
-}
-
-// Função para alternar a visibilidade da calculadora
-function toggleCalculator() {
-  const calculator = document.getElementById('calculator');
-  calculator.classList.toggle('hidden');
-}
-
-// Função para ativar/desativar o modo escuro
-function toggleDarkMode() {
-  const body = document.body;
-  body.classList.toggle('dark-mode');
-}
-
-// Função para exibir o PDF selecionado
-function showPDF(file) {
-  const reader = new FileReader();
-  reader.onload = function (e) {
-    const pdfContainer = document.getElementById('pdf-container');
-    pdfContainer.innerHTML = '';
-    const canvas = document.createElement('canvas');
-    pdfContainer.appendChild(canvas);
-    const pdfData = new Uint8Array(e.target.result);
-    PDFJS.getDocument(pdfData).then(function (pdf) {
-      pdf.getPage(1).then(function (page) {
-        const scale = 1.5;
-        const viewport = page.getViewport({ scale: scale });
-        const context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-        const renderContext = {
-          canvasContext: context,
-          viewport: viewport,
-        };
-        page.render(renderContext);
-      });
-    });
-  };
-  reader.readAsArrayBuffer(file);
-}
-
-// Evento do botão "Dark Mode"
-document.getElementById('dark-mode-button').addEventListener('click', toggleDarkMode);
-
-// Evento do botão "Adicionar" da checklist
-document.getElementById('add-item-button').addEventListener('click', addItem);
-
-// Evento do botão "Iniciar" do cronômetro
-document.getElementById('start-timer-button').addEventListener('click', startTimer);
-
-// Evento do botão "Parar" do cronômetro
-document.getElementById('stop-timer-button').addEventListener('click', stopTimer);
-
-// Evento do botão "Reiniciar" do cronômetro
-document.getElementById('reset-timer-button').addEventListener('click', resetTimer);
-
-// Evento do botão "Cronômetro"
-document.getElementById('toggle-timer-button').addEventListener('click', toggleTimer);
-
-// Evento do botão "Calculadora"
-document.getElementById('toggle-calculator-button').addEventListener('click', toggleCalculator);
-
-// Evento do input de seleção de arquivo
-document.getElementById('file-input').addEventListener('change', function (e) {
-  const file = e.target.files[0];
-  if (file) {
-    showPDF(file);
+// Inicia o temporizador
+let timerInterval;
+let startTime;
+let elapsedTime = 0;
+function startTimer() {
+  if (!timerInterval) {
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(function() {
+      const now = Date.now();
+      elapsedTime = now - startTime;
+      const formattedTime = formatTime(elapsedTime);
+      timerDisplay.textContent = formattedTime;
+    }, 10);
   }
+}
+
+// Para o temporizador
+function stopTimer() {
+  if (timerInterval) {
+    clearInterval(timerInterval);
+    timerInterval = null;
+  }
+}
+
+// Reinicia o temporizador
+function resetTimer() {
+  stopTimer();
+  elapsedTime = 0;
+  timerDisplay.textContent = '00:00:00.000';
+}
+
+// Formata o tempo em horas:minutos:segundos.milissegundos
+function formatTime(time) {
+  const hours = Math.floor(time / 3600000);
+  const minutes = Math.floor((time % 3600000) / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+  const milliseconds = time % 1000;
+
+  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(3, '0')}`;
+}
+
+// Adiciona um número ou operador ao campo de entrada da calculadora
+function addToCalcInput(value) {
+  calcInput.value += value;
+}
+
+// Calcula o resultado da expressão matemática no campo de entrada da calculadora
+function calculate() {
+  try {
+    const result = eval(calcInput.value);
+    calcInput.value = result;
+  } catch (error) {
+    calcInput.value = 'Error';
+  }
+}
+
+// Limpa o campo de entrada da calculadora
+function clearCalcInput() {
+  calcInput.value = '';
+}
+
+// Adiciona os eventos aos botões de número e operador da calculadora
+const calcButtonsList = Array.from(calcButtons.querySelectorAll('button'));
+calcButtonsList.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    const value = event.target.textContent;
+    addToCalcInput(value);
+  });
 });
-
-// Atualizar a hora a cada segundo
-setInterval(updateTime, 1000);
-          
+  
